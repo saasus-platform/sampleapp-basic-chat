469700f0f8cb4447a1185967b533fb65cf13d360
ttakuro <pochomukinsan@gmail.com>
2022-12-22 04:42:27 -0800

    migrated sdk into express app

--- node/express/api/controllers/chat.ts
+++ node/express/api/controllers/chat.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from "express";
 import { UpdateMeteringUnitDateCountTodayParam } from "saasus-sdk/dist/generated/Pricing";
-const db = require("../../models/index");
+const db = require("../models/index");
 import { findUpperCountByMeteringUnitName, PricingClient } from "saasus-sdk";
+import { PLANS } from "../controllers/plan";
+import { TENANT_NAME } from "../controllers/tenant";
 
-const getBoard = async (req: Request, res: Response) => {
+const getChats = async (req: Request, res: Response) => {
   try {
     const messages = await db.Messages.findAll({
       where: {
         tenant_id: req.userInfo?.tenants[0].id,
       },
     });
-    return res.json(messages);
+    res.render("chat", {
+      messages: messages,
+      plans: PLANS,
+      tenant_name: TENANT_NAME,
+    });
   } catch (error) {
     console.error(error);
-    return res
-      .status(500)
-      .send({ redirect_url: process.env.SAASUS_LOGIN_URL || "" });
+    res.redirect("/chat");
   }
 };
 
-const post = async (req: Request, res: Response) => {
+const postChats = async (req: Request, res: Response) => {
   const mes = req.body.message;
   const tenantId = req.userInfo?.tenants[0].id || "";
   const planId = req.userInfo?.tenants[0].plan_id || "";
@@ -60,6 +64,7 @@ const post = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
   }
+  res.redirect("/chat");
 };
 
-export { getBoard, post };
+export { getChats, postChats };
